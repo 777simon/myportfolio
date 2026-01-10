e9dc871a1f61aa4fa3fc85900866d3fd0b0ad61e
simon macharia <simon7chris777@gmail.com>
2026-01-10 16:06:23 +0300

    x

--- build_files.sh
+++ build_files.sh
@@ -2,13 +2,13 @@
 
 echo " BUILD START "
 
-# Install dependencies
+# Install the necessary libraries
 python3.12 -m pip install -r requirements.txt
 
-# Manually create the output directory to ensure Vercel sees it
+# Create the folder manually to be safe
 mkdir -p staticfiles_build/static
 
-# Collect static files into the specific directory defined in settings.py
+# Run collectstatic to fill that folder
 python3.12 manage.py collectstatic --noinput --clear
 
 echo " BUILD END "
