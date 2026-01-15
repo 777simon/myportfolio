87d183216f76fc46d510c3f5f1c50836ef5fe05b
simon macharia <simon7chris777@gmail.com>
2026-01-16 00:45:52 +0300

    x

--- build_files.sh
+++ build_files.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 echo "BUILD START"
 
-# Install dependencies
-python3.12 -m pip install -r requirements.txt
+pip install -r requirements.txt
 
-# Clean old builds and create the EXACT path expected by STATIC_ROOT
-mkdir -p staticfiles_build
-
-# Collect static files
-python3.12 manage.py collectstatic --noinput
+python manage.py collectstatic --noinput --clear
 
 echo "BUILD END"
