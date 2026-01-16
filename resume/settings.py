ca318fe27e7b226e3d3fa0e7e95b49da7c228305
simon macharia <simon7chris777@gmail.com>
2026-01-16 10:17:54 +0300

    x

--- resume/settings.py
+++ resume/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -135,7 +136,3 @@ STATICFILES_DIRS = [
 # This helps WhiteNoise serve files efficiently
 STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
 WHITENOISE_USE_FINDERS = True
-
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
-
--- resume/wsgi.py
+++ resume/wsgi.py
@@ -1,21 +1,14 @@
-"""
-WSGI config for resume project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
-
 from whitenoise import WhiteNoise
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'resume.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "resume.settings")
 
 application = get_wsgi_application()
 
-application = WhiteNoise(application, root=os.path.join(os.path.dirname(__file__), "staticfiles_build"))
-app = application
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+application = WhiteNoise(
+    application,
+    root=os.path.join(BASE_DIR, "staticfiles_build")
+)
