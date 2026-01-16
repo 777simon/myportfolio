a72c67ff72462b102a4ea0d8a308945f635aeea0
simon macharia <simon7chris777@gmail.com>
2026-01-16 10:22:17 +0300

    x

--- resume/wsgi.py
+++ resume/wsgi.py
@@ -4,11 +4,16 @@ from whitenoise import WhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "resume.settings")
 
+# Create Django WSGI application
 application = get_wsgi_application()
 
+# Point WhiteNoise to the CORRECT staticfiles_build location
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 application = WhiteNoise(
     application,
     root=os.path.join(BASE_DIR, "staticfiles_build")
 )
+
+# 🔴 REQUIRED BY VERCEL
+app = application
